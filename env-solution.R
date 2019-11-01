08a364167fa4c2e197a9a6e1e63ab47677cf3168
Marianna Plesiak <plesiakmarianna207@ukr.net>
2019-11-01 18:34:51 +0100

    ALL DONE

---
+++ env-solution.R
@@ -0,0 +1,16 @@
+########################## a) ##############################
+
+# environment:
+# - every name is unique
+# - names are not ordered
+# - has a parent environment
+# - does not include values of the objects but only their memory pointers
+
+########################## b) ##############################
+
+# global environment (calling environment?)
+
+########################## c) ##############################
+
+# <- : in a global envoronment
+# <<- : in a parent environment
